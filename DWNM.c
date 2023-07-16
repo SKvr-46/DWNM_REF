bd97695b4d95559cdbb746eccb75665611b84ae6
SKvr-46 <koeich4@gmail.com>
2023-07-17 01:03:38 +0900

    add phi prog

--- DWNM.c
+++ DWNM.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <complex.h>
 #include <math.h>
-#include <gsl/gsl_sf_bessel.h>
 
 #define N 20001  // Adjust this to match your data size
-#define L M_PI
-#define ALPHA 1.0
-#define R 1.0
+#define L 25.0
+#define ALPHA 5.0
+#define R 10.0
 #define Z 1.0
-#define N_RANGE 100000
+#define N_RANGE 1000
+#define f_s 500.0
+#define t_fin 20.0
+#define dt 1/f_s
+#define OMEGA_IMAG (-1j * 2 * M_PI / 1000)
 
-double complex u_values_omega_domain(double omegas[], int size) {
-    double complex u_values[N];
+double t[N];
+double omegas[N];
+
+double complex* u_values_omega_domain(double complex omegas[], int size) {
+    double complex* u_values = malloc(size * sizeof(double complex));
     
     for (int i = 0; i < size; i++) {
-        double omega = omegas[i];
+        double complex omega = omegas[i];
         double complex u_sum = 0.0 + 0.0 * I;
         
         for (int n = 0; n < N_RANGE; n++) {
             double k = 2 * n * M_PI / L;
-            double nu = sqrt((omega / ALPHA) * (omega / ALPHA) - k * k);
-            if (nu != nu) {  // check for NaN (imaginary number in sqrt)
+            double complex nu_val = (omega / ALPHA) * (omega / ALPHA) - k * k;
+            double complex nu = cimag(sqrt(nu_val)) < 0 ? sqrt(nu_val) : -1 * sqrt(nu_val);
+            if (cimag(nu) > 0) {
                 printf("Error: imaginary number in square root\n");
                 break;
             }
             if (n == 0) {
-                u_sum += (I * M_PI / L) * (k * k / nu) * gsl_sf_bessel_J1(k * R) * cexp(-I * nu * fabs(Z));
+                u_sum += (I * M_PI / L) * (k * k / nu) * j1(k * R) * cexp(-I * nu * fabs(Z));
             } else {
-                u_sum += (2 * (I * M_PI / L) * (k * k / nu) * gsl_sf_bessel_J1(k * R) * cexp(-I * nu * fabs(Z)));
+                u_sum += (2 * (I * M_PI / L) * (k * k / nu) * j1(k * R) * cexp(-I * nu * fabs(Z)));
             }
         }
         u_values[i] = u_sum;
@@ -52,10 +59,12 @@ void r_Fourier(double complex X[], int size) {  // Fourier inverse transformatio
     FILE *gnuplot = popen("gnuplot", "w");
     fprintf(gnuplot, "set term png\n");
     fprintf(gnuplot, "set output 'output.png'\n");
+    fprintf(gnuplot, "set xlabel 'time t'\n");
+    fprintf(gnuplot, "set ylabel 'u'\n");
     fprintf(gnuplot, "plot '-' with lines\n");
     
     for(n = 0; n < N; n++) {
-        fprintf(gnuplot, "%d %f\n", n, x_r[n]);
+        fprintf(gnuplot, "%f %f\n", t[n], x_r[n]);
     }
     
     fprintf(gnuplot, "e\n");
@@ -65,12 +74,17 @@ void r_Fourier(double complex X[], int size) {  // Fourier inverse transformatio
 }
 
 int main() {
-    double omegas[N];
+    // Initialize the time array and frequency array
     for (int i = 0; i < N; i++) {
-        omegas[i] = i - 10000;
+        t[i] = i * dt;
+        omegas[i] = (i/N)*f_s - f_s/2;
     }
-    double complex *u_values = u_values_omega_domain(omegas, N);
+
+    double complex* u_values = u_values_omega_domain(omegas, N);
     r_Fourier(u_values, N);
+
+    // Don't forget to free the dynamically allocated memory
+    free(u_values);
 
     return 0;
 }
